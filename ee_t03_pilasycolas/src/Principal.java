afc45017b52b327850310ed82df8a62190a1d409
AntonioAzcona <tom.ramms.15@gmail.com>
2016-09-19 20:20:46 -0500

    Update Principal.java

--- ee_t03_pilasycolas/src/Principal.java
+++ ee_t03_pilasycolas/src/Principal.java
@@ -15,22 +15,30 @@ public class Principal {
     File f = new File(ruta);
     PilaGenerica p = new PilaGenerica();
     ColaGenerica c = new ColaGenerica();
+    ListaSimplementeLigada l = new ListaSimplementeLigada();
+    String cadena = "";
     public void leerArchivo() throws Exception
     {
          FileReader fr = new FileReader(f);
          BufferedReader br = new BufferedReader(fr);
          String renglon = br.readLine();
-         String cadena = "";
-         System.out.println("Entrada: ");
+         System.out.println("Entrada:");
          while(renglon != null){
             cadena += renglon;
-            System.out.println(renglon);
+             System.out.println(renglon);
             renglon = br.readLine();
          }
          System.out.println("Salida: ");
-         for(int i = 0; i < cadena.length(); i++){
-             //if(cadena.charAt(0) == '0')
-             //{ i++;
+         
+         if(cadena.charAt(0) == '0')
+         {
+             metodoPila();
+         }else if(cadena.charAt(0) == '1'){
+             metodoCola();
+         }
+         /*for(int i = 0; i < cadena.length(); i++){
+             if(cadena.charAt(0) == '0')
+             { 
                  if(cadena.charAt(i) == '0')
                  {
                      if(cadena.charAt(i) == '0')
@@ -39,9 +47,51 @@ public class Principal {
                        if(cadena.charAt(i) == '0'){
                           i++;
                           p.push(cadena.charAt(i));
+                          //l.insertaInicio(cadena.charAt(i));
                        }else{
                           p.push(cadena.charAt(i));
+                          //l.insertaInicio(cadena.charAt(i));
                        }
+                     }
+                 }else if(cadena.charAt(i) == '1')
+                     {
+                         System.out.println(p.pop());
+                         //l.eliminaUltimo();
+                     }else if(cadena.charAt(i) == '2')
+                     {
+                         p.peek();
+                         //l.getInicio();
+                     }
+             }else if(cadena.charAt(0) == '1'){
+                 if(cadena.charAt(i) == '0')
+                 {
+                     if(cadena.charAt(i) == '1')
+                     {
+                         i++;
+                       if(cadena.charAt(i) == '0'){
+                            i++;
+                          c.insertarCola(cadena.charAt(i));
+                       }else{
+                          c.insertarCola(cadena.charAt(i));
+                       }
+                     }
+                 }else if(cadena.charAt(i) == '1')
+                     {
+                         i++;
+                         //c.eliminarCola();
+                     }
+             }
+         }*/
+    }
+   public void metodoPila()
+   {
+      for(int i = 1; i < cadena.length(); i++){
+                 if(cadena.charAt(i) == '0')
+                 {
+                     i++;
+                     if(cadena.charAt(i) != '0')
+                     {
+                         p.push(cadena.charAt(i));                       
                      }
                  }else if(cadena.charAt(i) == '1')
                      {
@@ -50,23 +100,42 @@ public class Principal {
                      {
                          p.peek();
                      }
-             //}
-         }
-    }
+        }
+   }
+   public void metodoCola() throws Exception
+   {
+       for(int i = 1; i < cadena.length(); i++)
+       {
+            if(cadena.charAt(i) == '0')
+               {
+                  i++;
+                  if(cadena.charAt(i) != '0')
+                  {
+                       c.insertarCola(cadena.charAt(i));
+                  }
+                }else 
+                {
+                       System.out.println(c.eliminarCola());
+                }
+       }
+   }
 }
     // C:\\Users\\Antonio\\Documents\\TEC\\5° Semestre\\Estructura de Datos Leon\\ee_t03_pilasycolas\\datos.txt
 //ENTRADA:
 /*
-  0  
-  01
-  02
-  03  
-  1
-  1
-  2
+  0       1      
+  01      01
+  02      02 
+  03      03
+  1       1
+  1       1
+  2       1
   1
 SALIDA: 
-  1231
+  3       1
+  2       2
+  1       3
+  1
 */
 
 
